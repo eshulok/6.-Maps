12443f6c0cded20a698383a4c076c338136a3b38
Liz S <shulok@berkeley.edu>
2020-06-23 00:05:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#Maps are used to store multiple items, just like lists and tuples, but they are not ordered and do not allow duplicate values
+#Maps are useful when you want to store information about something
+#They are also called dictionaries because like a dictionary entry, you have a key (the unique item you look up, which is a word in a dictionary), and a value for that key (the definition of the word in a dictionary)
+#To create a map, we use curley braces and colons 
+movie_ratings = {'Toy Story' : 5, 'Monsters, Inc.' : 4, "Inside Out" : 5, "A Bug's Life" : 2, "Coco" : 5}
+print(movie_ratings)
+
+print(movie_ratings['Toy Story'])
